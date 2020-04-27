31b93de988c610a24a6675af35001843804f3809
Pedro Henrique <phjfarias@hotmail.com>
2020-04-26 22:14:48 -0300

    list childrens

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ tree.insertRight(root, 3)
 tree.insertLeft(root.left, 4)
 tree.insertRight(root.left, 5)
 tree.insertLeft(root.right, 6)
+tree.insertRight(root.left.right, 7)
 
-
+print('Is empty: ', tree.isEmpty())
 print('Tree size: ', tree.size)
 print('Root: ', root.element)
-print('Left: ', root.left.element)
+print('Left: ', tree.left(root).element)
+print('Children: ', tree.children(root))
 
-print('preOrder: ', tree.toStringPreOrder())
-print('posOrder: ', tree.toStringPosOrder())
+print('PreOrder: ', tree.toStringPreOrder())
+print('PosOrder: ', tree.toStringPosOrder())
 
 print('depth: ', tree.depth(root.left.left))
